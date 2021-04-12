6b633339af18e69ed4ccdd570df8d263675b4724
jonathanyvargas <Jonathanyvargas@gmail.com>
2021-04-12 12:51:48 -0700

    Added images, and function refresh, tool instructions and color options.

--- sketch.js
+++ sketch.js
@@ -1,46 +1,67 @@
 let cnv;
-let dragons = false;
+let dragons = [];
+let animating = false;
 
-function preload(){
 
-  for (let i = 0; i <= 15; i++){
-    dragons[i] = loadImage(`assets/dragonDrawings${i}.JPG`)
+function preload() {
+
+  for (let i = 0; i <= 5; i++) {
+    dragons[i] = loadImage(`Assets/Dragon_${i}.PNG`)
   }
 }
 
-  function setup() {
-    cnv = createCanvas(600, 600);
-    cnv.parent("#canvasDiv");
+function setup() {
+  cnv = createCanvas(500, 500);
+  cnv.parent("#canvasDiv");
 
-    background(220);
-    strokeWeight(5)
+  background(240);
+  strokeWeight(5)
+
+  imageMode(CENTER)
+
+
+}
 
 
 
+function randomizer() {
+  animating = false;
+  image(random(dragons), width / 2, height / 2);
+
+
+}
+
+function keyTyped() {
+
+  if (key === 's') {
+    saveCanvas('fileName', 'png');
+  } else if (key === 'Enter') {
+
+    randomizer();
+
+  } else if (key === 'r'){stroke(255, 0, 0)}
+  else if (key === 'g'){stroke(0, 255, 0)}
+  else if (key === 'b'){stroke(0, 0, 255)}
+  else if (key === 'k'){stroke(0)}
+
+  else if (key === '/'){
+
+refresh();
 
   }
 
-  function draw() {
-
-if (mouseIsPressed == true){
-  line(mouseX, mouseY, pmouseX, pmouseY);
-
 
 }
 
-}
-
-function keyTyped(){
-
-if (key === 's'){
-  saveCanvas('fileName', 'png');
-} else if (key === 'd'){
-
-background(255)
-
-}
+function draw() {
+    if (mouseIsPressed) {
+      line(mouseX, mouseY, pmouseX, pmouseY);
 
 
+    }
+  }
 
+  function refresh(){
+    window.location.reload("refresh")
 
-}
+  }
