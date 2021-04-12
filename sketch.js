b8528ae42efb237dfb50adb03a40276b0f0fbdb0
jonathanyvargas <Jonathanyvargas@gmail.com>
2021-04-12 08:51:13 -0700

    Added html and CSS
    Working on image loading assets

--- sketch.js
+++ sketch.js
@@ -1,7 +1,46 @@
-function setup() {
-  createCanvas(400, 400);
+let cnv;
+let dragons = false;
+
+function preload(){
+
+  for (let i = 0; i <= 15; i++){
+    dragons[i] = loadImage(`assets/dragonDrawings${i}.JPG`)
+  }
 }
 
-function draw() {
-  background(220);
+  function setup() {
+    cnv = createCanvas(600, 600);
+    cnv.parent("#canvasDiv");
+
+    background(220);
+    strokeWeight(5)
+
+
+
+
+  }
+
+  function draw() {
+
+if (mouseIsPressed == true){
+  line(mouseX, mouseY, pmouseX, pmouseY);
+
+
+}
+
+}
+
+function keyTyped(){
+
+if (key === 's'){
+  saveCanvas('fileName', 'png');
+} else if (key === 'd'){
+
+background(255)
+
+}
+
+
+
+
 }
